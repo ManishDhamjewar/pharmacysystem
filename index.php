5e18d11dc4060f9d751eba9173f27c1aea26a0c0
ManishDhamjewar <88621875+ManishDhamjewar@users.noreply.github.com>
2022-06-25 12:57:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,9 +22,9 @@
 
       //alert(xhttp.responseText);
       if(xhttp.responseText == "")
-        window.location.href = "http://localhost/Pharmacy-Management1/index.php";
+        window.location.href = "https://pharmacysystem.azurewebsites.net/index.php";
       if(xhttp.responseText == "true")
-        window.location.href = "http://localhost/Pharmacy-Management1/home.php";
+        window.location.href = "https://pharmacysystem.azurewebsites.net/home.php";
 
     </script>
   </head>
