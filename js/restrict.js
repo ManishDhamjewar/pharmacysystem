dd20b296dde8897c7dfbc2491a8c3725a9e4340c
ManishDhamjewar <88621875+ManishDhamjewar@users.noreply.github.com>
2022-06-25 09:45:18 +0530

    Update restrict.js

--- js/restrict.js
+++ js/restrict.js
@@ -8,7 +8,7 @@ xhttp.send();
 
 //alert(xhttp.responseText);
 if(xhttp.responseText == "")
-  window.location.href = "http://localhost/Pharmacy-Management/login.php";
+  window.location.href = "http://localhost/Pharmacy-Management1/index.html";
 
 if(xhttp.responseText == "false")
-  window.location.href = "http://localhost/Pharmacy-Management/login.php";
+  window.location.href = "http://localhost/Pharmacy-Management1/login.php";
